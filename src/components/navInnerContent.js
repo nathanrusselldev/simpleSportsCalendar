a61525a0e84155d680e72d39aadbf52cb8eb4f16
nathanrusselldev <nathanrussell.it@gmail.com>
2022-11-05 15:33:45 -0400

    adding sports nav and nav basics

---
+++ src/components/navInnerContent.js
@@ -0,0 +1,24 @@
+const navInnerContent = {
+    nfl: {
+        content: `NFL`,
+        sendTo: `/nfl`
+    },
+    mlb: {
+        content: `MLB`,
+        sendTo: `/mlb`
+    },
+    nhl: {
+        content: `NHL`,
+        sendTo: `/nhl`
+    },
+    nba: {
+        content: 'NBA',
+        sendTo: `/nba`,
+    },
+    mls: {
+        content: `MLS`,
+        sendTo: `/mls`
+    },
+}
+
+export default navInnerContent
