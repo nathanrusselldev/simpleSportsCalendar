29f17593d5e937a60b067c1e2272bce49096bb03
nathanrusselldev <nathanrussell.it@gmail.com>
2022-11-06 10:13:55 -0500

    css updates

--- src/components/SportsNav.jsx
+++ src/components/SportsNav.jsx
@@ -22,25 +22,19 @@ export default function Navbar() {
           axios.request(options).then(function (response) {
             setData(response)
             
-            // console.log(response.data.DATA[96].NAME_PART_2);
-            // console.log(response.data.DATA[96].EVENTS[0].AWAY_NAME);
-            // console.log(response.data.DATA[96].EVENTS[0].AWAY_IMAGES);
-            // console.log(response.data.DATA[96].EVENTS[0].HOME_NAME);
-            // console.log(response.data.DATA[96].EVENTS[0].HOME_IMAGES);
-            // console.log(response.data.DATA[96].EVENTS[0].START_TIME);
-            
           }).catch(function (error) {
             console.error(error);
           });
     }
-    useEffect(() => {
-        getData()
-    }, []) 
-
+    // useEffect(() => {
+    //     getData()
+    // }, []) 
+    console.log(data)
     useEffect(() => {
 
     })
-    console.log(activeLeague)
+    if(activeLeague )
+
         return (
             
             <div className="sportsNavDiv">
@@ -48,11 +42,11 @@ export default function Navbar() {
                     <NavButton onClick={() => setLeague("/nfl")} {...navInnerContent.nfl} />
                     <NavButton onClick={() => setLeague("/mlb")} {...navInnerContent.mlb} />
                     <NavButton onClick={() => setLeague("/nhl")}{...navInnerContent.nhl} />
-                    <NavButton data={data} onClick={() => setLeague("/nba")}{...navInnerContent.nba} />
+                    <NavButton onClick={() => setLeague("/nba")}{...navInnerContent.nba} />
                     <NavButton onClick={() => setLeague("/mls")}{...navInnerContent.mls} />
                 </nav>
                 <div className='scheduleRender'>
-
+            
                 </div>
             </div>
         )
